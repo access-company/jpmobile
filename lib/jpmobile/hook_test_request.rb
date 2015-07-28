7561d6b251605318fc0a6643b0aaa3f1406f1d72
aKojiWatanabe <koji.watanabe@access-company.com>
2015-07-28 10:31:42 +0900

    [Fix] Accommodate ruby 2.2 and Rails 3.2 on Heroku

--- lib/jpmobile/hook_test_request.rb
+++ lib/jpmobile/hook_test_request.rb
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-require 'action_controller/test_case'
-ActionController::TestRequest.send :include, Jpmobile::RequestWithMobileTesting
+#require 'action_controller/test_case'
+#ActionController::TestRequest.send :include, Jpmobile::RequestWithMobileTesting
